82182fc9ad31261ef6fb8d510c36f235ab7f5209
Hans Losche <hansloesche.art@gmail.com>
2020-11-08 22:00:17 -0500

    updated

--- main.py
+++ main.py
@@ -1,35 +1,36 @@
 import cv2 as cv
 import numpy as np
 
-
-
 haystack_img = cv.imread('farm.jpg', cv.IMREAD_UNCHANGED)
 needle_img = cv.imread('cabbage.jpg', cv.IMREAD_UNCHANGED)
 
 result = cv.matchTemplate(haystack_img, needle_img, cv.TM_CCOEFF_NORMED)
 
-# get the best match position
-min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
 
-print('Best match top left position:%s' % str(max_loc))
-print('Best match confidence: %s' % max_val)
+threshold = 0.5
+locations = np.where(result >= threshold)
+locations = list(zip(*locations[::-1]))
+print(locations)
 
+if locations:
+    print('Found needle.')
 
-threshold = 0.8
-if max_val >= threshold:
-    print('Found needle')
-
-    # get dimension of the needle image
     needle_w = needle_img.shape[1]
     needle_h = needle_img.shape[0]
+    line_color = (0, 255, 0)
+    line_type = cv.LINE_4
 
-    top_left = max_loc
-    bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
+    # Loop over all the locations and draw their rectangle
+    for loc in locations:
+        # Determine the box positions
+        top_left = loc
+        bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
+        # Draw the box
+        cv.rectangle(haystack_img, top_left, bottom_right, line_color, line_type)
 
-    cv.rectangle(haystack_img, top_left, bottom_right,
-                    color=(0, 255, 0), thickness=2, lineType=cv.LINE_4)
-    # cv.imshow('Result', haystack_img)
-    # cv.waitKey()
-    cv.imwrite('result.jpg', haystack_img)
+    cv.imshow('Matches', haystack_img)
+    cv.waitKey()
+    #cv.imwrite('result.jpg', haystack_img)
+
 else:
     print('Needle not found.')
