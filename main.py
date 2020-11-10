40c4a1debc3b0374ce3cfdd5cb3313d9d35b3142
Hans Losche <hansloesche.art@gmail.com>
2020-11-09 19:30:52 -0500

    updated

--- main.py
+++ main.py
@@ -1,36 +1,69 @@
 import cv2 as cv
 import numpy as np
 
-haystack_img = cv.imread('farm.jpg', cv.IMREAD_UNCHANGED)
-needle_img = cv.imread('cabbage.jpg', cv.IMREAD_UNCHANGED)
 
-result = cv.matchTemplate(haystack_img, needle_img, cv.TM_CCOEFF_NORMED)
+def findClickPositions(needle_img_path, haystack_img_path, threshold=0.5, debug_mode=None):
 
-
-threshold = 0.5
-locations = np.where(result >= threshold)
-locations = list(zip(*locations[::-1]))
-print(locations)
-
-if locations:
-    print('Found needle.')
+    haystack_img = cv.imread(needle_img_path, cv.IMREAD_UNCHANGED)
+    needle_img = cv.imread(haystack_img_path, cv.IMREAD_UNCHANGED)
 
     needle_w = needle_img.shape[1]
     needle_h = needle_img.shape[0]
-    line_color = (0, 255, 0)
-    line_type = cv.LINE_4
 
-    # Loop over all the locations and draw their rectangle
+    # Found best results in this case using TM_SQDIFF_NORMED
+    method = cv.TM_CCOEFF_NORMED
+    result = cv.matchTemplate(haystack_img, needle_img, method)
+
+    locations = np.where(result >= threshold)
+    locations = list(zip(*locations[::-1]))
+    #print(locations)
+
+    # first we need to create the list of [x, y, w, h] rectangles
+    rectangles = []
     for loc in locations:
-        # Determine the box positions
-        top_left = loc
-        bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
-        # Draw the box
-        cv.rectangle(haystack_img, top_left, bottom_right, line_color, line_type)
+        rect = [int(loc[0]), int(loc[1]), needle_w, needle_h]
+        rectangles.append(rect)
+        rectangles.append(rect)
 
-    cv.imshow('Matches', haystack_img)
-    cv.waitKey()
-    #cv.imwrite('result.jpg', haystack_img)
 
-else:
-    print('Needle not found.')
+    rectangles, weights = cv.groupRectangles(rectangles, 1, 0.5)
+    print(rectangles, "\n Lenght of :", len(rectangles))
+
+    points = []
+    if len(rectangles):
+        print('Found needle.')
+
+        line_color = (0, 255, 0)
+        line_type = cv.LINE_4
+        marker_color = (255, 0, 255)
+        marker_type = cv.MARKER_CROSS
+
+        # Loop over all the locations and draw their rectangle
+        for (x, y, w, h) in rectangles:
+
+            # Determine the center position
+            center_x = x + int(w/2)
+            center_y = y + int(h/2)
+
+            # save the points
+            points.append((center_x, center_y ))
+            if debug_mode == 'rectangles':
+                # Determine the box positions
+                top_left = (x, y)
+                bottom_right = (x + w, y + h)
+                # Draw the box
+                cv.drawMarker(haystack_img, (center_x, center_y), marker_color, marker_type)
+            elif debug_mode == 'points':
+                cv.drawMarker(haystack_img, (center_x, center_y), marker_color, marker_type)
+
+        if debug_mode:
+            cv.imshow('Matches', haystack_img)
+            cv.waitKey()
+            #cv.imwrite('result.jpg', haystack_img)
+
+    return points
+
+points = findClickPositions('farm.jpg', 'cabbage.jpg',
+                            threshold=0.46, debug_mode='points')
+
+print(points)
